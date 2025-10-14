9b49896333f243676e904f736f5b4d84db1682d7
SuperNinja AI <superninja@ninjatech.ai>
2025-10-14 10:12:49 +0000

    Convert Batch 4 Part 1: Simple interfaces and enums (15 files)
    Converted files:
    - MemoryPressureListener interface
    - MuteType enum
    - VoicePluginMessageType enum
    - SLAvatarGlobalColor enum
    - MeshIndex enum
    - Loadable interface with nested enum and interface
    - SLMessageEventListener interface with abstract base class
    - Subscribable interface
    - RequestOperator abstract class
    - ResultOperator abstract class
    - LLSDUndefined class
    - AssetFormatException class
    - VoicePluginMessage interface
    - VoiceLogout class
    
    Progress: 15 of 74 remaining files converted (20% of final batch)

---
+++ app/src/main/java/com/lumiyaviewer/lumiya/react/Subscribable.kt
@@ -0,0 +1,15 @@
+package com.lumiyaviewer.lumiya.react
+
+import com.lumiyaviewer.lumiya.react.Subscription.OnData
+import com.lumiyaviewer.lumiya.react.Subscription.OnError
+import java.util.concurrent.Executor
+
+interface Subscribable<K, T> {
+    fun subscribe(k: K, onData: OnData<T>): Subscription<K, T>
+
+    fun subscribe(k: K, onData: OnData<T>, onError: OnError?): Subscription<K, T>
+
+    fun subscribe(k: K, executor: Executor?, onData: OnData<T>): Subscription<K, T>
+
+    fun subscribe(k: K, executor: Executor?, onData: OnData<T>, onError: OnError?): Subscription<K, T>
+}
---
+++ app/src/main/java/com/lumiyaviewer/lumiya/slproto/assets/AssetFormatException.kt
@@ -0,0 +1,13 @@
+package com.lumiyaviewer.lumiya.slproto.assets
+
+class AssetFormatException : Exception {
+    constructor() : super("Unsupported asset format")
+
+    constructor(str: String) : super(str)
+
+    constructor(str: String, th: Throwable) : super(str, th)
+
+    companion object {
+        private const val serialVersionUID = -8391424207465457690L
+    }
+}
